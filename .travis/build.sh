084c2549efcc4265724e9bc48f4956defb6f3217
Daniel Halperin <dan@intentionet.com>
2018-08-03 09:32:26 -0700

    Add travis configuration

---
+++ .travis/build.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -xe
+
+BATFISH_VERSION="$(grep -1 batfish-parent ${TRAVIS_BUILD_DIR}/batfish/projects/pom.xml | grep version | sed -E 's/.*<version>(.*)<\/version>.*/\1/g')"
+echo "Using Batfish version ${BATFISH_VERSION}"
+
+if [[ $(uname) == 'Darwin' && $(which gfind) ]]; then
+   GNU_FIND=gfind
+else
+   GNU_FIND=find
+fi
+
+if [ -n "$TRAVIS_BUILD_DIR" ]; then
+   # Build and install pybatfish
+   pip install -e .[dev,test]
+   export QUESTIONS_DIR=questions
+else
+   export QUESTIONS_DIR=../batfish/questions
+fi
+
+ALLINONE_JAR=${TRAVIS_BUILD_DIR}/batfish/projects/allinone/target/allinone-bundle-${BATFISH_VERSION}.jar
+
+# Start this as early as possible so that batfish has time to start up.
+if [ -n "$TRAVIS_BUILD_DIR" ]; then
+  # Only run allinone during Travis build. For local testing it should be running in background.
+  java -cp "$ALLINONE_JAR" org.batfish.allinone.Main -runclient false -coordinatorargs "-templatedirs questions" &
+fi
+
+# We can only run mypy using python 3. That's ok.
+# Our type annotations are py2-compliant and will be checked by mypy.
+if [[ $TRAVIS_PYTHON_VERSION != 2.7 ]]; then
+    echo -e "\n  ..... Running mypy typechecker on pybatfish"
+    mypy pybatfish
+    mypy --py2 pybatfish
+fi
+
+echo -e "\n  ..... Running flake8 on pybatfish to check style and docstrings"
+# All flake config in setup.cfg, but exclude cloned batfish folder
+flake8
+
+echo -e "\n  ..... Running flake8 on jupyter notebooks"
+# Running flake test on generated python script from jupyter notebook(s)
+for file in jupyter_notebooks/*.ipynb; do
+    jupyter nbconvert "$file" --to python --stdout --TemplateExporter.exclude_markdown=True | flake8 - --ignore=E501,W391,D100
+done
+
+
+### Run unit tests that don't require running instance of batfish
+echo -e "\n  ..... Running unit tests with pytest"
+python setup.py test
+
+#### Running integration tests (require batfish)
+echo -e "\n  ..... Running python ref tests with batfish"
+retcode=0
+py.test tests/integration || retcode=$?
+
+exit ${retcode}
---
+++ .travis/install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -x -e
+
+#### Before we begin, install Z3 for Ubuntu 14.04
+Z3_INSTALL_URL="https://raw.githubusercontent.com/batfish/batfish/master/tools/install_z3_ubuntu.sh"
+Z3_CMD_NAME=$(basename ${Z3_INSTALL_URL})
+wget ${Z3_INSTALL_URL}
+sudo bash ${Z3_CMD_NAME} /usr
+
+#### Next, install the correct version of Batfish -- either from a branch or TAG
+if [[ ${TRAVIS_BRANCH} =~ ^release.* ]]; then
+   BATFISH_BRANCH="$(echo ${TRAVIS_BRANCH} | sed -E 's/(release-[0-9]+\.[0-9]+).*/\1/')"
+else
+   BATFISH_BRANCH=master
+fi
+ 
+#### Checkout and install batfish
+git clone --depth=1 --branch=${BATFISH_BRANCH} https://github.com/batfish/batfish $TRAVIS_BUILD_DIR/batfish
+pushd "$TRAVIS_BUILD_DIR/batfish/projects"
+mvn clean install
+popd 
+
+# Symlink batfish testrigs to make them available to pybatfish
+ln -s "$TRAVIS_BUILD_DIR/batfish/test_rigs" $TRAVIS_BUILD_DIR
+
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+   ### install python packages
+   echo -e "\n   ............. Installing pip"
+   sudo -H apt-get -y install python-pip || exit 1
+   pip --version || exit 1
+elif [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+   brew update || exit 1
+   which gfind || brew install findutils || exit 1
+   gfind --version || exit 1
+   echo $PATH
+   export PATH=/usr/local/share/python:$PATH
+   java -version || exit 1
+   javac -version || exit 1
+   which pip || easy_install pip || exit 1
+   pip --version || exit 1
+else
+   echo "Unsupported TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
+   exit 1 # CI not supported in this case
+fi
+
+#### create symbolic links to stable and experimental questions
+ln -s $TRAVIS_BUILD_DIR/batfish/questions .
