6ace5e0e70991c5d3942798153c8c5bebbfd9fd4
Spencer Fraint <spencer@intentionet.com>
2018-08-03 13:59:57 -0700

    Add notebook startup file (#8)

---
+++ jupyter_notebooks/startup.py
@@ -0,0 +1,12 @@
+# Importing required libraries, setting up logging, and loading questions
+from pybatfish.client.commands import (bf_init_network, bf_set_network,  # noqa F401
+                                       bf_init_snapshot, bf_generate_dataplane,
+                                       bf_logger)
+from pybatfish.question import bfq, load_questions  # noqa F401
+from pybatfish.exception import BatfishException  # noqa F401
+
+import logging
+
+bf_logger.setLevel(logging.WARN)
+
+load_questions()
