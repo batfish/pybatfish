2ce2076077e33537164005a115b34e6296cf6a3b
Spencer Fraint <spencer@intentionet.com>
2018-08-03 12:44:26 -0700

    flake fix

--- jupyter_notebooks/startup.py
+++ jupyter_notebooks/startup.py
@@ -1,13 +1,12 @@
 # Importing required libraries, setting up logging, and loading questions
 from pybatfish.client.commands import (bf_init_network, bf_set_network,
                                        bf_init_snapshot, bf_generate_dataplane,
-                                       bf_logger)
-from pybatfish.question import bfq, load_questions
-from pybatfish.exception import BatfishException
+                                       bf_logger)  # noqa F401
+from pybatfish.question import bfq, load_questions  # noqa F401
+from pybatfish.exception import BatfishException  # noqa F401
 
 import logging
 
 bf_logger.setLevel(logging.WARN)
 
 load_questions()
-
