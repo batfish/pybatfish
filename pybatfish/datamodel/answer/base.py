59293d6c30abc82a8e1d9c803cabd49702d5b69d
ratul <ratul@intentionet.com>
2018-08-10 01:01:22 -0700

    adding support for selfdescribingobject schemas

--- pybatfish/datamodel/answer/base.py
+++ pybatfish/datamodel/answer/base.py
@@ -154,6 +154,8 @@ def _get_display_value(schema, json_object):
         return json_object["name"]
     if schema == "Prefix":
         return str(json_object)
+    if schema == "SelfDescribingObject":
+        return _get_display_value(json_object["schema"], json_object["value"])
     if schema == "String":
         return str(json_object)
     return json_object
---
+++ tests/datamodel/answer/test_base.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+#   Copyright 2018 The Batfish Open Source Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""Tests for Table answers."""
+
+from __future__ import absolute_import, print_function
+
+import json
+
+import pytest
+
+from pybatfish.datamodel.answer.base import _get_display_value
+
+
+def test_get_display_value_self_describing_object():
+    """Check that SelfDescribingObject is displayed correctly"""
+    json_object = json.loads("{\"schema\" : \"Integer\", \"value\": 23}")
+    assert _get_display_value('SelfDescribingObject', json_object) == 23
+
+
+def test_get_display_value_integer():
+    """Check that Integer values are extracted correctly."""
+    assert _get_display_value('Integer', "0") == 0
+    assert _get_display_value('Integer', 0) == 0
+    assert _get_display_value('Integer', -1) == -1
+    assert _get_display_value('Integer', "-1") == -1
+
+
+def test_get_display_value_unknown_schema():
+    """Check that Integer values are extracted correctly."""
+    assert _get_display_value('bogus', None) is None
+    json_object = json.loads("{\"foo\" : 23}")
+    assert _get_display_value('bogus', json_object) == json_object
+
+
+if __name__ == "__main__":
+    pytest.main()
