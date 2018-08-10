5e7e0499cece11cb65c9bdb501b171a3cc686d65
Corina Miner <c.miner19@gmail.com>
2018-08-09 18:51:34 -0700

    review improvements

--- jupyter_notebooks/startup.py
+++ jupyter_notebooks/startup.py
@@ -2,9 +2,11 @@
 from pybatfish.client.commands import *
 from pybatfish.question import bfq, load_questions  # noqa F401
 from pybatfish.exception import BatfishException  # noqa F401
+import pandas as pd
 
 import logging
 
 bf_logger.setLevel(logging.WARN)
 
 load_questions()
+pd.set_option('max_colwidth',250)
