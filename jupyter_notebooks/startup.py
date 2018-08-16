fc847aa2554592aed3b2fadb42bd54309735e9c5
ratul <ratul@intentionet.com>
2018-08-15 22:06:52 -0700

    changes startup

--- jupyter_notebooks/startup.py
+++ jupyter_notebooks/startup.py
@@ -1,6 +1,6 @@
 # Importing required libraries, setting up logging, and loading questions
 from pybatfish.client.commands import *
-from pybatfish.question import bfq, load_questions  # noqa F401
+from pybatfish.question import bfq, load_questions, list_questions  # noqa F401
 from pybatfish.exception import BatfishException  # noqa F401
 
 import logging
@@ -9,6 +9,7 @@ import pandas as pd
 bf_logger.setLevel(logging.WARN)
 
 load_questions()
+list_questions()
 
 pd.compat.PY3 = True
-pd.set_option('max_colwidth',250)
+pd.set_option('max_colwidth', 250)
