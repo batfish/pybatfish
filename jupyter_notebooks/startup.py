d242be5bbcfce6d2e11e610143e0d38b514ad568
ratul <ratul@intentionet.com>
2018-08-17 15:56:34 -0700

    review comments

--- jupyter_notebooks/startup.py
+++ jupyter_notebooks/startup.py
@@ -1,15 +1,15 @@
 # Importing required libraries, setting up logging, and loading questions
-from pybatfish.client.commands import *
-from pybatfish.question import bfq, load_questions, list_questions  # noqa F401
-from pybatfish.exception import BatfishException  # noqa F401
-
 import logging
+
 import pandas as pd
+
+from pybatfish.client.commands import *
+# noinspection PyUnresolvedReferences
+from pybatfish.question import bfq, list_questions, load_questions  # noqa F401
 
 bf_logger.setLevel(logging.WARN)
 
 load_questions()
-list_questions()
 
 pd.compat.PY3 = True
 pd.set_option('max_colwidth', 250)
