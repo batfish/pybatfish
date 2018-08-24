0e49d2cc49ebf598cdc35af53235a9eddbec94a1
Harsh Verma <harsh@intentionet.com>
2018-08-24 16:47:29 -0700

    Script to generate output for notebooks (#69)
    * making test for notebook output dump the updated output as well

--- tests/integration/test_notebook.py
+++ tests/integration/test_notebook.py
@@ -13,7 +13,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 from copy import deepcopy
-from os import walk
+from os import remove, walk
 from os.path import abspath, dirname, join, pardir, realpath
 
 from nbconvert.preprocessors import ExecutePreprocessor
@@ -32,22 +32,28 @@ notebook_files = [
     if '.ipynb_checkpoints' not in root and filename.endswith('.ipynb')
 ]
 
+for root, dirs, files in walk(_jupyter_nb_dir):
+    for filename in files:
+        if filename.endswith('.testout'):
+            remove(join(root, filename))
+
+
 assert len(notebook_files) > 0
 
 
 @pytest.fixture(scope='module', params=notebook_files)
 def notebook(request):
-    filename = request.param
-    return filename, nbformat.read(filename, as_version=4)
+    filepath = request.param
+    return filepath, nbformat.read(filepath, as_version=4)
 
 
 @pytest.fixture(scope='module')
 def executed_notebook(notebook):
-    filename, orig_nb = notebook
-    filename, nb = notebook  # Make a deep copy of the original notebook.
+    filepath, orig_nb = notebook
+    filepath, nb = notebook  # Make a deep copy of the original notebook.
     # - This is important or else the underlying object gets mutated!!
     nb = deepcopy(orig_nb)
-    exec_path = dirname(filename)
+    exec_path = dirname(filepath)
     # Run all cells in the notebook, with a time bound, continuing on errors
     ep = ExecutePreprocessor(timeout=60, allow_errors=True,
                              kernel_name="python3" if PY3 else "python2")
@@ -84,20 +90,26 @@ def test_notebook_no_errors(executed_notebook):
 
 
 def test_notebook_output(notebook, executed_notebook):
-    _, nb = notebook
-    for cell, executed_cell in zip(nb['cells'],
-                                   executed_notebook['cells']):
-        assert cell['cell_type'] == executed_cell['cell_type']
-        if cell['cell_type'] == 'code':
-            # Collecting all outputs of type "execute_result" as other output type may be undeterministic (like timestamps)
-            original_outputs = [o['data'] for o in cell['outputs']
-                                if o['output_type'] == 'execute_result']
-            executed_outputs = [o['data'] for o in executed_cell['outputs']
-                                if o['output_type'] == 'execute_result']
-            assert len(original_outputs) == len(executed_outputs)
-            for original_data, executed_data in zip(original_outputs,
-                                                    executed_outputs):
-                _compare_data(original_data, executed_data)
+    filepath, nb = notebook
+    try:
+        for cell, executed_cell in zip(nb['cells'],
+                                       executed_notebook['cells']):
+            assert cell['cell_type'] == executed_cell['cell_type']
+            if cell['cell_type'] == 'code':
+                # Collecting all outputs of type "execute_result" as other output type may be undeterministic (like timestamps)
+                original_outputs = [o['data'] for o in cell['outputs']
+                                    if o['output_type'] == 'execute_result']
+                executed_outputs = [o['data'] for o in executed_cell['outputs']
+                                    if o['output_type'] == 'execute_result']
+                assert len(original_outputs) == len(executed_outputs)
+                for original_data, executed_data in zip(original_outputs,
+                                                        executed_outputs):
+                    _compare_data(original_data, executed_data)
+    except AssertionError as e:
+        with open('{}.testout'.format(filepath), 'w') as f:
+            nbformat.write(executed_notebook, f)
+            pytest.fail('{} failed output validation:\n{}'.format(filepath, e),
+                        pytrace=False)
 
 
 def test_notebook_execution_count(notebook):
