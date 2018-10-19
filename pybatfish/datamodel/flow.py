9b8510934fdd326b9286b1b2c70c26d519608be3
Harsh Verma <harsh@intentionet.com>
2018-10-18 18:34:24 -0700

    Adding space in Hop step representation (#159)

--- pybatfish/datamodel/flow.py
+++ pybatfish/datamodel/flow.py
@@ -351,8 +351,8 @@ class Hop(DataModelElement):
     @staticmethod
     def _get_step_data_(step):
         # type: (Dict) -> str
-        return "{type}({action})".format(type=step.get("type"),
-                                         action=step.get("action"))
+        return "{type} ({action})".format(type=step.get("type"),
+                                          action=step.get("action"))
 
 
 @attr.s(frozen=True)
