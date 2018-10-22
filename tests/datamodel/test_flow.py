bb379de7be8bd62339685f4e9576f3c8195cbe84
Harsh Verma <harsh@intentionet.com>
2018-10-22 16:45:15 -0700

    remove trace helpers and tests (#162)

--- tests/datamodel/test_flow.py
+++ tests/datamodel/test_flow.py
@@ -18,7 +18,7 @@ import attr
 import pytest
 
 from pybatfish.datamodel.flow import (Flow, FlowTraceHop, HeaderConstraints,
-                                      Hop, Trace)
+                                      )
 
 
 def testFlowDeserialization():
@@ -172,52 +172,6 @@ def test_header_constraints_serialization():
 
     with pytest.raises(ValueError):
         HeaderConstraints(applications="")
-
-
-def test_trace():
-    trace = Trace(disposition='accepted', hops=[])
-    assert len(trace) == 0
-    with pytest.raises(IndexError):
-        assert trace[0]
-
-    trace = Trace(disposition='accepted', hops=[Hop('node1', [])])
-    assert len(trace) == 1
-    assert len(trace[0]) == 0
-
-    trace = Trace(disposition='accepted',
-                  hops=[Hop('node1',
-                            [{'action': 'secret_action'}])])
-    assert trace.final_detail() == {}
-
-    trace = Trace(disposition='accepted',
-                  hops=[Hop('node1',
-                            [{'action': 'secret_action', 'detail': 'secret'}])])
-    assert trace.final_detail() == 'secret'
-
-
-def test_disposition_detail():
-    trace = Trace(disposition="DENIED_IN", hops=[Hop('node1', [
-        {'action': 'BLOCKED', 'detail': {'inputInterface': 'in_iface1',
-                                         'inputFilter': 'in_filter1'}}])])
-    assert trace.disposition_reason() == "Flow was BLOCKED at in_iface1 by filter in_filter1"
-
-
-def test_get_all_filters():
-    trace = Trace(disposition="ACCEPTED", hops=[
-        Hop('node1', [
-            {'action': 'SENT_IN', 'detail': {'inputInterface': 'in_iface1',
-                                             'inputFilter': 'in_filter1'}},
-            {'action': 'FORWARDED', 'detail': {'routes': []}},
-            {'action': 'SENT_OUT', 'detail': {'outputInterface': 'out_iface1',
-                                              'outputFilter': 'out_filter1'}}]),
-        Hop('node2', [
-            {'action': 'SENT_IN', 'detail': {'inputInterface': 'in_iface2',
-                                             'inputFilter': 'in_filter2'}},
-            {'action': 'FORWARDED', 'detail': {'routes': []}},
-            {'action': 'SENT_OUT', 'detail': {'outputInterface': 'out_iface2',
-                                              'outputFilter': 'out_filter2'}}])
-    ])
-    assert trace.get_all_filters() == ['in_filter1', 'out_filter1', 'in_filter2', 'out_filter2']
 
 
 if __name__ == "__main__":
