a0a10c8b6f70aae8dc5283baf498f9f6b102764c
Daniel Halperin <dan@intentionet.com>
2019-02-06 11:07:14 -0800

    fix erroneous understanding

--- .buildkite/build_batfish.sh
+++ .buildkite/build_batfish.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 BF_DIR=$(mktemp -d)
 git clone --depth=1 --branch=master https://github.com/batfish/batfish ${BF_DIR}
-mvn -f ${BF_DIR}/batfish/projects package
-cp ${BF_DIR}/batfish/projects/allinone/target/allinone-bundle-*.jar workspace/allinone.jar
+mvn -f ${BF_DIR}/projects package
+cp ${BF_DIR}/projects/allinone/target/allinone-bundle-*.jar workspace/allinone.jar
