4ffe7e01fd2699dc1649349f71ea248207297117
Daniel Halperin <dan@intentionet.com>
2019-02-06 00:38:11 -0800

    add pytest back to tests_require

--- setup.py
+++ setup.py
@@ -137,6 +137,7 @@ setup(
 
     # List pytest requirements for running unit tests
     setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
