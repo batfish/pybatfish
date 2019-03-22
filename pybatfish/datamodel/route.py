a9b1677bfab26489d2297f2f8c717de921edeae8
Matt Brown <matt@intentionet.com>
2019-03-22 12:41:49 -0700

    new datamodel classes for BgpRoute and differences between BgpRoutes

---
+++ pybatfish/datamodel/route.py
@@ -0,0 +1,139 @@
+# coding=utf-8
+#   Copyright 2018 The Batfish Open Source Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+from typing import Dict, List
+
+import attr
+from pybatfish.datamodel.primitives import DataModelElement
+
+__all__ = [
+    "BgpRoute",
+    "BgpRouteDiff",
+    "BgpRouteDiffs"
+]
+
+
+@attr.s(frozen=True)
+class BgpRoute(DataModelElement):
+    """A BGP routing advertisement.
+
+    :ivar network: The network prefix advertised by the route.
+    :ivar asPath: The AS path of the route.
+    :ivar communities: The communities of the route.
+    :ivar localPreference: The local preference of the route.
+    :ivar metric: The metric of the route.
+    :ivar originatorIp: The IP address of the originator of the route.
+    :ivar originType: The origin type of the route.
+    """
+
+    network = attr.ib(type=str)
+    originatorIp = attr.ib(type=str)
+    originType = attr.ib(type=str)
+    protocol = attr.ib(type=str)
+    asPath = attr.ib(type=list, default=[])
+    communities = attr.ib(type=list, default=[])
+    localPreference = attr.ib(type=int, default=0)
+    metric = attr.ib(type=int, default=0)
+
+    @classmethod
+    def from_dict(cls, json_dict):
+        # type: (Dict) -> BgpRoute
+        return BgpRoute(json_dict["network"],
+                        json_dict["originatorIp"],
+                        json_dict["originType"],
+                        json_dict["protocol"],
+                        json_dict.get("asPath", []),
+                        json_dict.get("communities", []),
+                        json_dict.get("localPreference", 0),
+                        json_dict.get("metric", 0))
+
+    def dict(self):
+        return {
+            'class': 'org.batfish.datamodel.BgpRoute',
+            'network': self.network,
+            'asPath': self.asPath,
+            'communities': self.communities,
+            'localPreference': self.localPreference,
+            'metric': self.metric,
+            'originatorIp': self.originatorIp,
+            'originType': self.originType,
+            'protocol': self.protocol
+        }
+
+    def _repr_html_(self):
+        # type: () -> str
+        return '<br>'.join(self._repr_html_lines())
+
+    def _repr_html_lines(self):
+        # type: () -> List[str]
+        lines = []
+        lines.append('Network: {node}'.format(node=self.network))
+        lines.append('AS Path: %s' % self.asPath)
+        lines.append('Communities: %s' % self.communities)
+        lines.append('Local Preference: %s' % self.localPreference)
+        lines.append('Metric: %s' % self.metric)
+        lines.append('Originator IP: %s' % self.originatorIp)
+        lines.append('Origin Type: %s' % self.originType)
+        lines.append('Protocol: %s' % self.protocol)
+        return lines
+
+
+@attr.s(frozen=True)
+class BgpRouteDiff(DataModelElement):
+    """A difference between two BGP routes.
+
+    :ivar fieldName: A Flow field name that has changed.
+    :ivar oldValue: The old value of the field.
+    :ivar newValue: The new value of the field.
+    """
+
+    fieldName = attr.ib(type=str)
+    oldValue = attr.ib(type=str)
+    newValue = attr.ib(type=str)
+
+    @classmethod
+    def from_dict(cls, json_dict):
+        # type: (Dict) -> BgpRouteDiff
+        return BgpRouteDiff(json_dict["fieldName"],
+                            json_dict["oldValue"],
+                            json_dict["newValue"])
+
+    def _repr_html_(self):
+        # type: () -> str
+        return "{fieldName}: {oldValue} -> {newValue}".format(
+            fieldName=self.fieldName,
+            oldValue=self.oldValue,
+            newValue=self.newValue)
+
+
+@attr.s(frozen=True)
+class BgpRouteDiffs(DataModelElement):
+    """A set of differences between two BGP routes.
+
+    :ivar diffs: The set of BgpRouteDiff objects.
+    """
+
+    diffs = attr.ib(type=list)
+
+    @classmethod
+    def from_dict(cls, json_dict):
+        # type: (Dict) -> BgpRouteDiffs
+        return BgpRouteDiffs(
+            set(BgpRouteDiff.from_dict(route_dict)
+                for route_dict in json_dict['diffs']))
+
+    def _repr_html_(self):
+        # type: () -> str
+        return "<BR>".join(diff._repr_html_() for diff in self.diffs)
+
---
+++ tests/datamodel/test_route.py
@@ -0,0 +1,57 @@
+from pybatfish.datamodel.route import BgpRouteDiff, BgpRoute
+
+
+def testBgpRouteDeserialization():
+    network = '0.0.0.0/0'
+    asPath = [1, 2, 3]
+    communities = [4, 5, 6]
+    localPreference = 1
+    metric = 2
+    dct = {
+        'network': network,
+        'asPath': asPath,
+        'communities': communities,
+        'localPreference': localPreference,
+        'metric': metric,
+    }
+    bgpRoute = BgpRoute.from_dict(dct)
+    assert bgpRoute.network == network
+    assert bgpRoute.communities == communities
+    assert bgpRoute.localPreference == localPreference
+    assert bgpRoute.metric == metric
+
+
+def testBgpRouteStr():
+    bgpRoute = BgpRoute(
+        network="A",
+        asPath=[1, 2],
+        communities=[1, 2, 3],
+        localPreference=4,
+        metric=5)
+    assert bgpRoute._repr_html_lines() == [
+        "Network: A",
+        "AS Path: [1, 2]",
+        "Communities: [1, 2, 3]",
+        "Local Preference: 4",
+        "Metric: 5"
+        ]
+
+
+def testBgpRouteDiffDeserialization():
+    name = 'communities'
+    oldValue = 'old'
+    newValue = 'new'
+    dct = {
+        'fieldName': name,
+        'oldValue': oldValue,
+        'newValue': newValue,
+    }
+    routeDiff = BgpRouteDiff.from_dict(dct)
+    assert routeDiff.fieldName == name
+    assert routeDiff.oldValue == oldValue
+    assert routeDiff.newValue == newValue
+
+
+def testBgpRouteDiffStr():
+    diff = BgpRouteDiff(fieldName='nm', oldValue='old', newValue='new')
+    assert str(diff) == "nm: old -> new"
