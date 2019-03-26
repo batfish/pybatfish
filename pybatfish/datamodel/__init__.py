7afdff717a612b3274425cdfe59e7864bd6d6c77
Matt Brown <matt@intentionet.com>
2019-03-25 17:40:58 -0700

    more flake

--- pybatfish/datamodel/__init__.py
+++ pybatfish/datamodel/__init__.py
@@ -14,6 +14,6 @@
 
 from .acl import *  # noqa: F401,F403
 from .flow import *  # noqa: F401,F403
-from .route import *  # noqa: F401,F403
 from .primitives import *  # noqa: F401,F403
 from .referencelibrary import *  # noqa: F401,F403
+from .route import *  # noqa: F401,F403
