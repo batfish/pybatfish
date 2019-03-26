0e789d14f10aebad25973268b511d40fccad1e76
Matt Brown <matt@intentionet.com>
2019-03-26 09:38:11 -0700

    fix unit tests

--- tests/datamodel/test_route.py
+++ tests/datamodel/test_route.py
@@ -7,18 +7,28 @@ def testBgpRouteDeserialization():
     communities = [4, 5, 6]
     localPreference = 1
     metric = 2
+    originType = 'egp'
+    originatorIp = '1.1.1.1'
+    protocol = 'bgp'
+
     dct = {
         'network': network,
         'asPath': asPath,
         'communities': communities,
         'localPreference': localPreference,
         'metric': metric,
+        'originType': originType,
+        'originatorIp': originatorIp,
+        'protocol' : protocol
     }
     bgpRoute = BgpRoute.from_dict(dct)
     assert bgpRoute.network == network
     assert bgpRoute.communities == communities
     assert bgpRoute.localPreference == localPreference
     assert bgpRoute.metric == metric
+    assert bgpRoute.originType == originType
+    assert bgpRoute.originatorIp == originatorIp
+    assert bgpRoute.protocol == protocol
 
 
 def testBgpRouteStr():
@@ -27,14 +37,21 @@ def testBgpRouteStr():
         asPath=[1, 2],
         communities=[1, 2, 3],
         localPreference=4,
-        metric=5)
-    assert bgpRoute._repr_html_lines() == [
-        "Network: A",
-        "AS Path: [1, 2]",
-        "Communities: [1, 2, 3]",
-        "Local Preference: 4",
-        "Metric: 5"
-    ]
+        metric=5,
+        originatorIp='1.1.1.1',
+        originType='egp',
+        protocol='bgp'
+    )
+    lines = bgpRoute._repr_html_lines()
+    assert len(lines) == 8
+    assert lines[0] == "Network: A"
+    assert lines[1] == "AS Path: [1, 2]"
+    assert lines[2] == "Communities: [1, 2, 3]"
+    assert lines[3] == "Local Preference: 4"
+    assert lines[4] == "Metric: 5"
+    assert lines[5] == "Originator IP: 1.1.1.1"
+    assert lines[6] == "Origin Type: egp"
+    assert lines[7] == "Protocol: bgp"
 
 
 def testBgpRouteDiffDeserialization():
@@ -54,4 +71,4 @@ def testBgpRouteDiffDeserialization():
 
 def testBgpRouteDiffStr():
     diff = BgpRouteDiff(fieldName='nm', oldValue='old', newValue='new')
-    assert str(diff) == "nm: old -> new"
+    assert diff._repr_html_() == "nm: old -> new"
