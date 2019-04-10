e6efd00351020ef19fb1c6ffd0f1836331b65b04
Dan Halperin <dhalperi@users.noreply.github.com>
2019-04-10 11:41:23 -0700

    MatchTcpFlags: add more helpers (#337)

--- pybatfish/datamodel/flow.py
+++ pybatfish/datamodel/flow.py
@@ -759,19 +759,47 @@ class MatchTcpFlags(DataModelElement):
     @staticmethod
     def match_ack():
         # type: () -> MatchTcpFlags
-        """Return match conditions checking that ACK bit is set."""
+        """Return match conditions checking that ACK bit is set.
+
+        Other bits may take any value.
+        """
         return MatchTcpFlags(TcpFlags(ack=True), useAck=True)
 
     @staticmethod
     def match_rst():
         # type: () -> MatchTcpFlags
-        """Return match conditions checking that RST bit is set."""
+        """Return match conditions checking that RST bit is set.
+
+        Other bits may take any value.
+        """
         return MatchTcpFlags(TcpFlags(rst=True), useRst=True)
+
+    @staticmethod
+    def match_syn():
+        # type: () -> MatchTcpFlags
+        """Return match conditions checking that the SYN bit is set.
+
+        Other bits may take any value.
+        """
+        return MatchTcpFlags(TcpFlags(syn=True), useSyn=True)
+
+    @staticmethod
+    def match_synack():
+        # type: () -> MatchTcpFlags
+        """Return match conditions checking that both the SYN and ACK bits are set.
+
+        Other bits may take any value.
+        """
+        return MatchTcpFlags(
+            TcpFlags(ack=True, syn=True), useAck=True, useSyn=True)
 
     @staticmethod
     def match_established():
         # type: () -> List[MatchTcpFlags]
-        """Return a list of match conditions matching an established flow (ACK or RST bit set)."""
+        """Return a list of match conditions matching an established flow (ACK or RST bit set).
+
+        Other bits may take any value.
+        """
         return [MatchTcpFlags.match_ack(), MatchTcpFlags.match_rst()]
 
 
