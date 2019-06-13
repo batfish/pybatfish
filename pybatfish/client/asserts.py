250b80238e6cde5a790971515e78a9b7f8d5c8c8
Spencer Fraint <spencer@intentionet.com>
2019-06-13 12:53:28 -0700

    use original name

--- pybatfish/client/asserts.py
+++ pybatfish/client/asserts.py
@@ -43,7 +43,7 @@ __all__ = [
     'assert_flows_succeed',
     'assert_has_no_route',
     'assert_has_route',
-    'assert_no_matching_bgp_sessions',
+    'assert_no_incompatible_bgp_sessions',
     'assert_no_undefined_references',
     'assert_num_results',
     'assert_zero_results',
@@ -313,14 +313,12 @@ def assert_flows_succeed(startLocation, headers, soft=False, snapshot=None,
     return True
 
 
-def assert_no_matching_bgp_sessions(nodes=None, remote_nodes=None,
-                                    status=_INCOMPATIBLE_BGP_SESSION_STATUS_REGEX,
-                                    snapshot=None,
-                                    soft=False, session=None):
+def assert_no_incompatible_bgp_sessions(nodes=None, remote_nodes=None,
+                                        status=_INCOMPATIBLE_BGP_SESSION_STATUS_REGEX,
+                                        snapshot=None,
+                                        soft=False, session=None):
     # type: (Optional[str], Optional[str], str, Optional[str], bool, Optional[Session]) -> bool
-    """Assert that there are no matching BGP sessions present in the snapshot.
-
-    By default, checks that there are no incompatible BGP sessions.
+    """Assert that there are no incompatible BGP sessions present in the snapshot.
 
     :param nodes: search sessions with specified nodes on one side of the sessions.
     :param remote_nodes: search sessions with specified remote_nodes on other side of the sessions.
