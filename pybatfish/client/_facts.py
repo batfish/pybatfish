82e2748c5668ecbd09405a1703adfb3e05e0a778
Spencer Fraint <spencer@intentionet.com>
2019-06-24 16:04:23 -0700

    Add fact extraction and validation (#403)

---
+++ pybatfish/client/_facts.py
@@ -0,0 +1,318 @@
+# coding=utf-8
+#   Copyright 2018 The Batfish Open Source Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+import os
+from collections import Mapping
+from copy import deepcopy
+from typing import Any, Dict, Optional, TYPE_CHECKING, Text, Tuple
+
+import yaml
+
+from pybatfish.datamodel import ListWrapper
+from pybatfish.datamodel.answer import TableAnswer
+
+if TYPE_CHECKING:
+    from pybatfish.client.session import Session  # noqa: F401
+
+BATFISH_FACT_VERSION = "batfish_v0"
+
+# Map of property name to new parent key
+NODE_PROPERTIES_REORG = dict([
+    ('DNS_Servers', 'DNS'),
+    ('DNS_Source_Interface', 'DNS'),
+    ('IKE_Phase1_Keys', 'IPsec'),
+    ('IKE_Phase1_Policies', 'IPsec'),
+    ('IKE_Phase1_Proposals', 'IPsec'),
+    ('IPsec_Peer_Configs', 'IPsec'),
+    ('IPsec_Phase2_Policies', 'IPsec'),
+    ('IPsec_Phase2_Proposals', 'IPsec'),
+    ('NTP_Servers', 'NTP'),
+    ('NTP_Source_Interface', 'NTP'),
+    ('Logging_Servers', 'Syslog'),
+    ('Logging_Source_Interface', 'Syslog'),
+    ('SNMP_Source_Interface', 'SNMP'),
+    ('SNMP_Trap_Servers', 'SNMP'),
+    ('TACACS_Servers', 'TACACS'),
+    ('TACACS_Source_Interface', 'TACACS'),
+])  # type: Dict[Text, Text]
+
+
+def get_facts(session, nodes_specifier):
+    # type: (Session, Text) -> Dict[Text, Any]
+    """Get current-snapshot facts from the specified session for nodes matching the nodes specifier."""
+    args = {}
+    if nodes_specifier:
+        args['nodes'] = nodes_specifier
+
+    node_properties = session.q.nodeProperties(  # type: ignore
+        **args).answer()
+    interface_properties = session.q.interfaceProperties(  # type: ignore
+        **args).answer()
+    bgp_process_properties = session.q.bgpProcessConfiguration(  # type: ignore
+        **args).answer()
+    bgp_peer_properties = session.q.bgpPeerConfiguration(  # type: ignore
+        **args).answer()
+
+    return _encapsulate_nodes_facts(_process_facts(node_properties,
+                                                   interface_properties,
+                                                   bgp_process_properties,
+                                                   bgp_peer_properties),
+                                    BATFISH_FACT_VERSION)
+
+
+def load_facts(input_directory):
+    # type: (Text) -> Dict[Text, Any]
+    """Load facts from text files in the specified directory."""
+    out = {'version': None, 'nodes': {}}  # type: Dict[Text, Any]
+    out_nodes = out['nodes']
+
+    files = os.listdir(input_directory)
+    if not len(files):
+        raise ValueError('No files present in specified directory')
+
+    for filename in files:
+        with open(os.path.join(input_directory, filename), 'r') as f:
+            dict_ = yaml.safe_load(f.read())
+            nodes, version = _unencapsulate_facts(dict_)
+
+            # Assume latest version if none is specified
+            if not version:
+                version_text = BATFISH_FACT_VERSION
+            else:
+                version_text = version
+
+            # Make sure version is consistent
+            if out['version'] and version_text != out['version']:
+                raise ValueError('Input file version mismatch!')
+            out['version'] = version_text
+
+            # We allow a single input file to specify more than one node
+            for node in nodes:
+                out_nodes[node] = nodes[node]
+    return out
+
+
+def validate_facts(expected, actual):
+    # type: (Dict[Text, Any], Dict[Text, Any]) -> Dict[Text, Any]
+    """Return a map of node to non-matching facts based on the difference between the expected and actual facts supplied."""
+    failures = {}
+    expected_facts = expected['nodes']
+    actual_facts = actual['nodes']
+    expected_version = expected['version']
+    actual_version = actual['version']
+    if expected_version != actual_version:
+        return {
+            n: {
+                'Version': {
+                    'expected': expected_version,
+                    'actual': actual_version
+                }
+            } for n in expected_facts
+        }
+    for node in actual_facts:
+        if node in expected_facts:
+            res = _assert_dict_subset(actual_facts[node],
+                                      expected_facts[node])
+            if res:
+                failures[node] = res
+    return failures
+
+
+def _process_facts(node_props, iface_props, bgp_process_props, bgp_peer_props):
+    # type: (TableAnswer, TableAnswer, TableAnswer, TableAnswer) -> Dict[Text, Any]
+    """Process properties answers into a fact dict."""
+    # out = {}
+    out = _process_nodes(node_props)
+    _process_interfaces(out, iface_props)
+    _process_bgp_processes(out, bgp_process_props)
+    _process_bgp_peers(out, bgp_peer_props)
+
+    return _convert_listwrapper(out)
+
+
+def _convert_listwrapper(dict_):
+    # type: (Mapping[Text, Any]) -> Dict[Text, Any]
+    """Convert ListWrapper objects in the specified dict into plain lists."""
+    out = {}  # type: Dict[Text, Any]
+    for k in dict_:
+        val = dict_[k]
+        if isinstance(val, Mapping):
+            out[k] = _convert_listwrapper(val)
+        elif isinstance(val, ListWrapper):
+            out[k] = list(val)
+        else:
+            out[k] = val
+    return out
+
+
+def _process_bgp_processes(node_dict, bgp_proc_props):
+    # type: (Dict[Text, Any], TableAnswer) -> None
+    """Return fact dict with processed bgp process properties answer added to it."""
+    bgp_proc_dict = bgp_proc_props.frame().to_dict(orient='records')
+    for r in bgp_proc_dict:
+        node = r.pop('Node')
+        # These will be replaced by bgp peers when we process them
+        r['Neighbors'] = {}
+        node_dict[node]['BGP'] = r
+
+
+def _process_bgp_peers(node_dict, bgp_peer_props):
+    # type: (Dict[Text, Any], TableAnswer) -> None
+    """Return fact dict with processed bgp peer properties answer added to it."""
+    bgp_peer_dict = bgp_peer_props.frame().to_dict(orient='records')
+    for r in bgp_peer_dict:
+        node = r.pop('Node')
+        ip = r.get('Remote_IP')
+        node_dict[node]['BGP']['Neighbors'][str(ip)] = r
+
+
+def _process_interfaces(node_dict, iface_props):
+    # type: (Dict[Text, Any], TableAnswer) -> None
+    """Return fact dict with processed interface properties answer added to it."""
+    iface_dict = iface_props.frame().to_dict(orient='records')
+    for i in iface_dict:
+        node = i['Interface'].hostname
+        if node in node_dict:
+            _add_interface(node_dict[node], i)
+        else:
+            # Should be impossible for ifaceProps to have node not in nodeProps
+            raise ValueError(
+                'Interface belongs to non-existent node {}'.format(node)
+            )
+
+
+def _process_nodes(node_props):
+    # type: (TableAnswer) -> Dict[Text, Any]
+    """Return fact dict corresponding to processed node properties answer."""
+    out = {}
+    node_dict = node_props.frame().to_dict(orient='records')
+    for record in node_dict:
+        node = record.pop('Node')
+        record.pop('Interfaces')
+
+        # Remove Batfish-constructed structures e.g.:
+        # routing policies like ~BGP_COMMON_EXPORT_POLICY:default~
+        _remove_constructed_names(record)
+
+        # Add properties as children of node
+        out[node] = record
+        _reorg_dict(out[node], NODE_PROPERTIES_REORG)
+    return out
+
+
+def _remove_constructed_names(dict_):
+    # type: (Dict[Text, Any]) -> None
+    """Remove names constructed by Batfish.
+
+    Removes strings beginning with ~ from lists in the specified dict.
+    """
+    for k in dict_:
+        if isinstance(dict_[k], list):
+            dict_[k] = [i for i in dict_[k] if not i.startswith('~')]
+
+
+def _add_interface(node_dict, iface_dict_in):
+    # type: (Dict[Text, Any], Dict[Text, Any]) -> None
+    """Update and add interface dict to the specified node dict."""
+    if 'Interfaces' not in node_dict:
+        node_dict['Interfaces'] = {}
+    iface_dict = deepcopy(iface_dict_in)
+
+    iface_name = iface_dict_in['Interface'].interface
+    iface_dict.pop('Interface')
+
+    node_dict['Interfaces'][iface_name] = iface_dict
+
+
+def _reorg_dict(dict, reorg_map):
+    # type: (Dict[Text, Any], Dict[Text, Text]) -> None
+    """Reorganize top-level keys in the input dict based on the supplied reorg map."""
+    for key in reorg_map:
+        new_parent = reorg_map[key]
+        if new_parent not in dict:
+            dict[new_parent] = {}
+        dict[new_parent][key] = dict.pop(key)
+
+
+def _write_yaml_file(dict_, filepath):
+    # type: (Dict[Text, Any], Text) -> None
+    with open(filepath, 'w') as f:
+        # default_flow_style=False dumps lists as hyphen lists instead of
+        # square bracket lists
+        yaml.safe_dump(dict_, stream=f, default_flow_style=False)
+
+
+def write_facts(output_directory, facts):
+    # type: (Text, Dict[Text, Any]) -> None
+    """Write facts to YAML files in the supplied output directory."""
+    nodes, version = _unencapsulate_facts(facts)
+
+    if version:
+        version_text = version
+    else:
+        raise ValueError('Version must be specified to write facts.')
+
+    # Write facts for each node in its own file
+    for node in nodes:
+        filepath = os.path.join(output_directory, '{}.yml'.format(node))
+        node_facts = _encapsulate_nodes_facts({node: facts['nodes'][node]},
+                                              version_text)
+        _write_yaml_file(node_facts, filepath)
+
+
+def _encapsulate_nodes_facts(nodes_facts, version):
+    # type: (Dict[Text, Any], Text) -> Dict[Text, Any]
+    """Format node(s) facts in final fact format (the way they are written to file)."""
+    return {
+        'nodes': nodes_facts,
+        'version': version,
+    }
+
+
+def _unencapsulate_facts(facts):
+    # type: (Dict[Text, Any]) -> Tuple[Any, Optional[Any]]
+    """Extract node facts and version from final fact format."""
+    assert 'nodes' in facts, 'No nodes present in parsed facts file(s)'
+    return facts['nodes'], facts.get('version')
+
+
+def _assert_dict_subset(actual, expected, prefix="", diffs=None):
+    # type: (Dict[Text, Any], Dict[Text, Any], Text, Optional[Dict]) -> Dict[Text, Any]
+    """Assert that the expected dictionary is a subset of the actual dictionary.
+
+    :param actual: the dictionary tested
+    :param expected: the expected value of a dictionary
+    """
+    if diffs is None:
+        diffs_out = {}  # type: Dict[Text, Any]
+    else:
+        diffs_out = diffs
+    for k in expected:
+        key_name = '{}{}'.format(prefix, k)
+        if k not in actual:
+            diffs_out[key_name] = {
+                'key_present': False,
+                'expected': expected[k],
+            }
+        else:
+            if isinstance(expected[k], Mapping):
+                _assert_dict_subset(actual[k], expected[k], key_name + '.',
+                                    diffs_out)
+            else:
+                if expected[k] != actual[k]:
+                    diffs_out[key_name] = {
+                        'expected': expected[k],
+                        'actual': actual[k],
+                    }
+    return diffs_out
---
+++ tests/client/test_facts.py
@@ -0,0 +1,307 @@
+# coding=utf-8
+#   Copyright 2018 The Batfish Open Source Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+import os
+
+import pytest
+import six
+import yaml
+from pandas import DataFrame
+
+from pybatfish.client._facts import (
+    _assert_dict_subset, _encapsulate_nodes_facts, _unencapsulate_facts,
+    get_facts, load_facts, validate_facts, write_facts
+)
+from pybatfish.client.session import Session
+from pybatfish.datamodel.answer import TableAnswer
+from pybatfish.question.question import QuestionBase
+
+if six.PY3:
+    from unittest.mock import patch
+else:
+    from mock import patch
+
+
+class MockTableAnswer(TableAnswer):
+    def __init__(self, frame_to_use=DataFrame()):
+        self._frame = frame_to_use
+
+    def frame(self):
+        return self._frame
+
+
+class MockQuestion(QuestionBase):
+    def __init__(self, answer=None):
+        self._answer = answer if answer is not None else MockTableAnswer()
+
+    def answer(self, *args, **kwargs):
+        return self._answer
+
+
+def test_get_facts_questions():
+    """Test that get facts calls the right questions, passing through the right args."""
+    bf = Session(load_questions=False)
+    nodes = 'foo'
+    with patch.object(bf.q,
+                      'nodeProperties',
+                      create=True) as mock_node, \
+            patch.object(bf.q,
+                         'interfaceProperties',
+                         create=True) as mock_iface, \
+            patch.object(bf.q,
+                         'bgpPeerConfiguration',
+                         create=True) as mock_peers, \
+            patch.object(bf.q,
+                         'bgpProcessConfiguration',
+                         create=True) as mock_proc:
+        mock_node.return_value = MockQuestion(MockTableAnswer())
+        mock_iface.return_value = MockQuestion(MockTableAnswer())
+        mock_proc.return_value = MockQuestion(MockTableAnswer())
+        mock_peers.return_value = MockQuestion(MockTableAnswer())
+        get_facts(bf, nodes)
+
+        mock_node.assert_called_with(nodes=nodes)
+        mock_iface.assert_called_with(nodes=nodes)
+        mock_proc.assert_called_with(nodes=nodes)
+        mock_peers.assert_called_with(nodes=nodes)
+
+
+def test_load_facts(tmpdir):
+    """Test that load_facts correctly loads facts from a fact directory."""
+    version = 'fake_version'
+    node1 = {
+        'node1': 'foo'
+    }
+    node2 = {
+        'node2': 'foo'
+    }
+    tmpdir.join('node1.yml').write(_encapsulate_nodes_facts(node1, version))
+    tmpdir.join('node2.yml').write(_encapsulate_nodes_facts(node2, version))
+    facts = load_facts(str(tmpdir))
+
+    # Confirm facts were loaded from both files
+    assert facts == _encapsulate_nodes_facts({'node1': 'foo', 'node2': 'foo'},
+                                             version)
+
+
+def test_load_facts_bad_dir(tmpdir):
+    """Test load facts when loading from bad directories."""
+    # Empty input dir should throw ValueError
+    with pytest.raises(ValueError) as e:
+        load_facts(str(tmpdir))
+    assert 'No files present in specified dir' in str(e)
+
+    f = tmpdir.join('file')
+    f.write('foo')
+    # File instead of dir should throw an exception about path not being a directory
+    with pytest.raises(Exception) as e:
+        load_facts(str(f))
+    assert 'Not a directory' in str(e)
+
+
+def test_load_facts_mismatch_version(tmpdir):
+    """Test load facts when loaded nodes have different format versions."""
+    version1 = 'version1'
+    node1 = {
+        'node1': 'foo'
+    }
+    version2 = 'version2'
+    node2 = {
+        'node2': 'foo'
+    }
+    tmpdir.join('node1.yml').write(_encapsulate_nodes_facts(node1, version1))
+    tmpdir.join('node2.yml').write(_encapsulate_nodes_facts(node2, version2))
+    with pytest.raises(ValueError) as e:
+        load_facts(str(tmpdir))
+    assert 'Input file version mismatch' in str(e)
+
+
+def test_validate_facts():
+    """Test that fact validation works for matching facts."""
+    expected = {
+        'node1': {'foo': 1},
+        'node2': {'foo': 2},
+    }
+    actual = {
+        'node1': {'foo': 1},
+        'node2': {'foo': 2},
+        'node3': {'foo': 3},
+    }
+    version = 'fake_version'
+    res = validate_facts(_encapsulate_nodes_facts(expected, version),
+                         _encapsulate_nodes_facts(actual, version))
+    # No results from matching (subset) expected and actual
+    assert len(res) == 0
+
+
+def test_validate_facts_not_matching_version():
+    """Test that fact validation detects mismatched versions."""
+    expected = {
+        'node1': {'foo': 1},
+        'node2': {'foo': 2},
+    }
+    actual = {
+        'node1': {'foo': 1},
+        'node2': {'foo': 2},
+        'node3': {'foo': 3},
+    }
+    version_expected = 'correct_version'
+    version_actual = 'fake_version'
+    res = validate_facts(_encapsulate_nodes_facts(expected, version_expected),
+                         _encapsulate_nodes_facts(actual, version_actual))
+    # One result per expected node for mismatched version
+    assert len(res) == len(expected)
+    for node in expected:
+        # Make sure version mismatch details are in results details
+        assert res[node] == {
+            'Version': {
+                'actual': version_actual,
+                'expected': version_expected,
+            }
+        }
+
+
+def test_validate_facts_not_matching_data():
+    """Test that fact validation works for mismatched facts."""
+    expected = {
+        'node1': {'foo': 1, 'bar': 1, 'baz': 1},
+        'node2': {'foo': 2},
+    }
+    actual = {
+        'node1': {'foo': 0, 'bar': 1},  # 'foo' doesn't match expected
+        # also missing 'baz': 1
+        'node2': {'foo': 2},
+        'node3': {'foo': 3},
+    }
+    version = 'version'
+    res = validate_facts(_encapsulate_nodes_facts(expected, version),
+                         _encapsulate_nodes_facts(actual, version))
+
+    # Result should identify the mismatched value and the missing key
+    assert res['node1'] == {
+        'foo': {
+            'expected': 1,
+            'actual': 0,
+        },
+        'baz': {
+            'expected': 1,
+            'key_present': False,
+        }
+    }
+
+
+def test_write_facts(tmpdir):
+    """Test that writing facts writes nodes' facts to individual files."""
+    nodes = {'node1': 'foo', 'node2': 'bar'}
+    version = 'version'
+    facts = _encapsulate_nodes_facts(nodes, version)
+    write_facts(str(tmpdir), facts)
+    for node in nodes:
+        filename = node + '.yml'
+        file_path = str(tmpdir.join(filename))
+        assert os.path.isfile(file_path)
+        with open(file_path) as f:
+            node_facts_raw = yaml.safe_load(f.read())
+            node_facts, node_version = _unencapsulate_facts(node_facts_raw)
+            assert version == node_version, 'Each file has the correct version'
+            assert node_facts.get(node) == nodes[
+                node], 'Each file has the correct facts'
+
+
+def test_assert_dict_subset_equal():
+    """Test that assert_dict_subset correctly identifies equal dicts."""
+    actual = {
+        'key': 'value',
+        'parent_key': {
+            'nested_key': 'nested_value',
+        },
+        'list': ['foo'],
+        'empty_list': [],
+        'none': None,
+    }
+    expected = {
+        'key': 'value',
+        'parent_key': {
+            'nested_key': 'nested_value',
+        },
+        'list': ['foo'],
+        'empty_list': [],
+        'none': None,
+    }
+    # Equal dicts should result in no differences
+    assert _assert_dict_subset(actual, expected) == {}
+
+
+def test_assert_dict_subset_subset():
+    """Test that assert_dict_subset correctly identifies expected as a subset of actual."""
+    actual = {
+        'key': 'value',
+        'key2': 'value2',
+        'parent_key': {
+            'nested_key': 'nested_value',
+            'nested_key2': 'nested_value2',
+        },
+    }
+    expected = {
+        'key': 'value',
+        'parent_key': {
+            'nested_key': 'nested_value',
+        },
+    }
+    # Expected being a subset should result in no differences
+    assert _assert_dict_subset(actual, expected) == {}
+
+
+def test_assert_dict_subset_not_equal():
+    """Test that assert_dict_subset correctly identifies when expected is not a subset of actual."""
+    actual = {
+        'key': 'value',
+        'key2': 'value2',
+        'parent_key': {
+            'nested_key': 'nested_value',
+            'nested_key2': 'nested_value2',
+            'different_nested_key': 'not_different_value',
+        },
+        'different_key': 'not_different_value',
+    }
+    expected = {
+        'key': 'value',
+        'parent_key': {
+            'nested_key': 'nested_value',
+            'missing_nested_key': 'missing_value',
+            'different_nested_key': 'different_value',
+        },
+        'missing_key': 'missing_value',
+        'different_key': 'different_value',
+
+    }
+    # Make sure we identify missing and different values
+    assert _assert_dict_subset(actual, expected) == {
+        'parent_key.missing_nested_key': {
+            'expected': 'missing_value',
+            'key_present': False,
+        },
+        'parent_key.different_nested_key': {
+            'expected': 'different_value',
+            'actual': 'not_different_value',
+        },
+        'missing_key': {
+            'expected': 'missing_value',
+            'key_present': False,
+        },
+        'different_key': {
+            'expected': 'different_value',
+            'actual': 'not_different_value',
+        }
+    }
