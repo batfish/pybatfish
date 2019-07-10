615b8ea3e8c6c1e28daef2711fa18d40d104b0c9
ratulm <ratul@intentionet.com>
2019-07-10 02:58:49 -0700

    flake

--- pybatfish/datamodel/referencelibrary.py
+++ pybatfish/datamodel/referencelibrary.py
@@ -12,10 +12,10 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import six
 from typing import Any, Dict, List  # noqa: F401
 
 import attr
+import six
 
 from .primitives import DataModelElement, Interface
 
