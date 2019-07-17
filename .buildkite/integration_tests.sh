5487c2f8d0d7c88b24480a8f30b9e2bb3f25813c
Victor Heorhiadi <victor@intentionet.com>
2019-07-16 19:31:43 -0700

    Set source correctly

--- .buildkite/integration_tests.sh
+++ .buildkite/integration_tests.sh
@@ -8,6 +8,6 @@ apt update -qq && apt -qq install -y openjdk-11-jre-headless
 tar -xzf workspace/questions.tgz
 java -cp workspace/allinone.jar org.batfish.allinone.Main -runclient false -coordinatorargs '-templatedirs questions -periodassignworkms=5' 2>&1 > workspace/batfish.log &
 pip install -e .[dev] -q
-pytest tests/integration --cov=pybatfish
+pytest tests/integration 
 pytest docs pybatfish --doctest-glob='docs/source/*.rst' --doctest-modules
 bash <(curl -s https://codecov.io/bash) -t 91216eec-ae5e-4836-8ee5-1d5a71d1b5bc -F integration${version//.}
