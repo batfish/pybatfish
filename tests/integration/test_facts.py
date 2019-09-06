18cb1e3a07cbfeff0b433137a150d365c871bf53
Spencer Fraint <spencer@intentionet.com>
2019-09-06 13:25:33 -0700

    allow specifying snapshot name for fact extraction and validation

--- tests/integration/test_facts.py
+++ tests/integration/test_facts.py
@@ -22,13 +22,18 @@ from pybatfish.client.session import Session
 _this_dir = abspath(dirname(realpath(__file__)))
 _root_dir = abspath(join(_this_dir, pardir, pardir))
 
+OLD_SNAPSHOT_NAME = 'old_snapshot'
+
 
 @pytest.fixture()
 def session():
     s = Session()
-    name = s.init_snapshot(join(_this_dir, 'facts', 'fact_snapshot'))
+    s.init_snapshot(join(_this_dir, 'snapshots', 'fact_snapshot2'),
+                    OLD_SNAPSHOT_NAME)
+    name = s.init_snapshot(join(_this_dir, 'snapshots', 'fact_snapshot'))
     yield s
     s.delete_snapshot(name)
+    s.delete_snapshot(OLD_SNAPSHOT_NAME)
 
 
 def test_extract_facts(tmpdir, session):
@@ -46,10 +51,34 @@ def test_extract_facts(tmpdir, session):
                           written_facts) == {}, 'Written facts match expected facts'
 
 
+def test_extract_facts_specific_snapshot(tmpdir, session):
+    """Test extraction of facts for a specific snapshot with a basic config."""
+    out_dir = tmpdir.join('output')
+    extracted_facts = session.extract_facts(output_directory=str(out_dir),
+                                            snapshot=OLD_SNAPSHOT_NAME)
+
+    written_facts = load_facts(str(out_dir))
+    expected_facts = load_facts(join(_this_dir, 'facts', 'expected_facts2'))
+
+    assert validate_facts(expected_facts,
+                          extracted_facts) == {}, 'Extracted facts match expected facts'
+    assert validate_facts(expected_facts,
+                          written_facts) == {}, 'Written facts match expected facts'
+
+
 def test_validate_facts_matching(session):
     """Test validation of facts for the current snapshot against matching facts."""
     validation_results = session.validate_facts(
         expected_facts=join(_this_dir, 'facts', 'expected_facts'))
+
+    assert validation_results == {}, 'No differences between expected and actual facts'
+
+
+def test_validate_facts_matching_specific_snapshot(session):
+    """Test validation of facts for a specific snapshot against matching facts."""
+    validation_results = session.validate_facts(
+        expected_facts=join(_this_dir, 'facts', 'expected_facts2'),
+        snapshot=OLD_SNAPSHOT_NAME)
 
     assert validation_results == {}, 'No differences between expected and actual facts'
 
