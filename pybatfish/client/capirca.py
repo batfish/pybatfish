10454cf10e254979b403a537d7f6a61cd4a2a706
Dan Halperin <dhalperi@users.noreply.github.com>
2019-09-10 16:57:37 -0700

    Remove Capirca/absl-py workaround (#462)
    In the latest release of absl-py common library, Google fixed the logging
    mess. Force a minimum version of that library and remove the hack.
    
    https://github.com/abseil/abseil-py/issues/99

--- pybatfish/client/capirca.py
+++ pybatfish/client/capirca.py
@@ -30,25 +30,6 @@ except ImportError:
         'Capirca must be installed to use the Pybatfish Capirca extensions')
     raise
 
-try:
-    # Capirca uses Google's abseil-py library, which uses a Google-specific
-    # wrapper for logging. That wrapper will write a warning to sys.stderr if
-    # the Google command-line flags library has not been initialized.
-    #
-    # https://github.com/abseil/abseil-py/blob/pypi-v0.7.1/absl/logging/__init__.py#L819-L825
-    #
-    # This is not right behavior for Python code that is invoked outside of a
-    # Google-authored main program. Use knowledge of abseil-py to disable that
-    # warning; ignore and continue if something goes wrong.
-    import absl.logging
-
-    # https://github.com/abseil/abseil-py/issues/99
-    logging.root.removeHandler(absl.logging._absl_handler)
-    # https://github.com/abseil/abseil-py/issues/102
-    absl.logging._warn_preinit_stderr = False
-except Exception:
-    pass
-
 from pybatfish.datamodel import AddressGroup, ReferenceBook
 
 if TYPE_CHECKING:
