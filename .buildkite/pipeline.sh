e2c477002291ac5f11586638b4f6a0ba17f9b1c5
Victor Heorhiadi <victor@intentionet.com>
2019-12-16 13:16:59 -0800

    typo

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -23,7 +23,7 @@ cat <<EOF
     command:
       - "python3 -m virtualenv .venv"
       - ". .venv/bin/activate"
-      - "python3 -m pip install regex=2019.11.1 black"
+      - "python3 -m pip install regex==2019.11.1 black"
       - "./fix_format.sh --check"
     plugins:
       - docker#${BATFISH_DOCKER_PLUGIN_VERSION}:
