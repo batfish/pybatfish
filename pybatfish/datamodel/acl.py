7a141631c3f490033e083adf1bfebc15e43f2cb7
Wey-Han Liaw <weyhan@intentionet.com>
2020-01-14 13:36:41 -0800

    Remove importing typing.Iterator

--- pybatfish/datamodel/acl.py
+++ pybatfish/datamodel/acl.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from typing import Dict, List, Iterator, Optional  # noqa: F401
+from typing import Dict, List, Optional  # noqa: F401
 
 import attr
 
