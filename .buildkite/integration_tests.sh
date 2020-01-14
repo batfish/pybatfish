fd988f717426c119c0bedf19df2a64474a9fc35f
Victor Heorhiadi <victor@intentionet.com>
2020-01-14 10:55:27 -0800

    Fix up reqs

--- .buildkite/integration_tests.sh
+++ .buildkite/integration_tests.sh
@@ -10,6 +10,7 @@ java -cp workspace/allinone.jar org.batfish.allinone.Main -runclient false -coor
 pip install -e .[dev] -q
 pytest tests/integration
 if [[ $version != "3.5" ]]; then
+  pip install -r docs/requirements.txt
   pytest docs pybatfish --doctest-glob='docs/source/*.rst' --doctest-modules
 fi
 bash <(curl -s https://codecov.io/bash) -t 91216eec-ae5e-4836-8ee5-1d5a71d1b5bc -F integration${version//.}
