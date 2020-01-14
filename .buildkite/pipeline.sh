6e4fecad7a0af866829f398df019339e43c42c53
Victor Heorhiadi <victor@intentionet.com>
2020-01-14 13:47:57 -0800

    Ensure we can build documentation as part of pre-commit
    Minimal overhead, but ensures our documentation actually compiles

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -98,3 +98,19 @@ for version in ${PYBATFISH_PYTHON_TEST_VERSIONS[@]}; do
             - workspace/questions.tgz
 EOF
 done
+
+###### Test building of documentation
+cat <<EOF
+  - label: "Building documentation"
+    command:
+      - bash .buildkite/build_docs.sh
+    plugins:
+      - docker#${BATFISH_DOCKER_PLUGIN_VERSION}:
+          image: "python:3.7"
+          always-pull: true
+          propagate-environment: true
+      - artifacts#${BATFISH_ARTIFACTS_PLUGIN_VERSION}:
+          download:
+            - workspace/allinone.jar
+            - workspace/questions.tgz
+EOF
