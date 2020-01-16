ff55cb634d6051af82b1861a7c185fe34dee2911
Victor Heorhiadi <victor@intentionet.com>
2020-01-16 11:42:23 -0800

    New docs: add specifier documentation
    * Enables using markdown in sphinx builds w/ recommonmark
    * Add specifiers.md that is a mirror of parameters.md
    * Add test to check that batfish's parameters.md and specifiers.md is in sync

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,30 +35,33 @@ import pybatfish  # noqa: F402
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.doctest',
-              'sphinx.ext.todo',
-              'sphinx.ext.mathjax',
-              'sphinx.ext.viewcode',
-              'sphinx.ext.coverage',
-              'sphinx.ext.githubpages']
+extensions = [
+    "recommonmark",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.coverage",
+    "sphinx.ext.githubpages",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'pybatfish'
-copyright = '2016-2018, The Batfish Open Source Project'
-author = 'The Batfish Open Source Project'
+project = "pybatfish"
+copyright = "2016-2018, The Batfish Open Source Project"
+author = "The Batfish Open Source Project"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -82,7 +85,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
@@ -92,7 +95,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -124,7 +127,7 @@ html_static_path = []
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pybatfishdoc'
+htmlhelp_basename = "pybatfishdoc"
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -132,15 +135,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -150,18 +150,14 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'pybatfish.tex', 'pybatfish Documentation',
-     author, 'manual'),
+    (master_doc, "pybatfish.tex", "pybatfish Documentation", author, "manual")
 ]
 
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'pybatfish', 'pybatfish Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "pybatfish", "pybatfish Documentation", [author], 1)]
 
 # -- Options for Texinfo output -------------------------------------------
 
@@ -169,7 +165,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'pybatfish', 'pybatfish Documentation',
-     author, 'pybatfish', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "pybatfish",
+        "pybatfish Documentation",
+        author,
+        "pybatfish",
+        "One line description of project.",
+        "Miscellaneous",
+    )
 ]
