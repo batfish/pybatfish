46e71dc5157be51eab0681e930ac01cd0e22d2ce
Victor Heorhiadi <progwriter@users.noreply.github.com>
2020-01-29 12:29:28 -0800

    Test docs in separate pipeline step (#594)
    Hopefully will speed up the builds a bit

--- .buildkite/doc_tests.sh
+++ .buildkite/doc_tests.sh
@@ -2,11 +2,9 @@
 
 set -euo pipefail
 
-version=$1
-
 apt update -qq && apt -qq install -y openjdk-11-jre-headless
 tar -xzf workspace/questions.tgz
 java -cp workspace/allinone.jar org.batfish.allinone.Main -runclient false -coordinatorargs '-templatedirs questions -periodassignworkms=5' 2>&1 > workspace/batfish.log &
+pip install -r docs/requirements.txt
 pip install -e .[dev] -q
-pytest tests/integration
-bash <(curl -s https://codecov.io/bash) -t 91216eec-ae5e-4836-8ee5-1d5a71d1b5bc -F integration${version//.}
+pytest docs pybatfish --doctest-glob='docs/source/*.rst' --doctest-modules
