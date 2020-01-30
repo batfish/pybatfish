a687ff6749ae0bcd679463538d323d16d48ece0d
Victor Heorhiadi <victor@intentionet.com>
2020-01-30 10:59:52 -0800

    Move doctest out of unittests

--- docs/nb_gen_tests/test_specifiers_md.py
+++ docs/nb_gen_tests/test_specifiers_md.py
@@ -1,27 +1,10 @@
-# coding: utf-8
-
-#   Copyright 2018 The Batfish Open Source Project
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
 import hashlib
 import os
-from os.path import abspath, dirname, realpath
+from os.path import realpath, dirname, abspath
 from pathlib import Path
 
-import pytest
 import requests
 
-# Note that notebooks are tested in test_notebook.py
 _this_dir = Path(abspath(dirname(realpath(__file__))))
 _root_dir = _this_dir.parent.parent
 
