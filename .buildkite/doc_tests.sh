776212cdaac7a768d053a99628ab7522fc577589
Victor Heorhiadi <progwriter@users.noreply.github.com>
2020-02-04 14:19:00 -0800

    Fix doctests (#601)
    We were getting false positive passes.

--- .buildkite/doc_tests.sh
+++ .buildkite/doc_tests.sh
@@ -7,4 +7,5 @@ tar -xzf workspace/questions.tgz
 java -cp workspace/allinone.jar org.batfish.allinone.Main -runclient false -coordinatorargs '-templatedirs questions -periodassignworkms=5' 2>&1 > workspace/batfish.log &
 pip install -r docs/requirements.txt
 pip install -e .[dev] -q
-pytest docs pybatfish --doctest-glob='docs/source/*.rst' --doctest-modules
+pytest docs
+pytest pybatfish --doctest-modules
