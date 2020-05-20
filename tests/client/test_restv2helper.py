3cfac273ef8cef616ea42deb29d0a83fe847a1c0
Spencer Fraint <spencer@intentionet.com>
2020-05-20 14:59:28 -0700

    header helper and use correct requests session object

--- tests/client/test_restv2helper.py
+++ tests/client/test_restv2helper.py
@@ -12,10 +12,21 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+from unittest.mock import Mock, patch
+
 import pytest
+import responses
 from requests import HTTPError, Response
 
 from pybatfish.client import restv2helper
+from pybatfish.client.restv2helper import (
+    _delete,
+    _encoder,
+    _get,
+    _get_headers,
+    _post,
+    _put,
+)
 
 
 class MockResponse(Response):
@@ -40,6 +51,107 @@ def test_check_response_status_ok():
     response = MockResponse("no error")
     response.status_code = 200
     restv2helper._check_response_status(response)
+
+
+@responses.activate
+def test_delete():
+    """Make sure calls to _delete end up using the correct session."""
+    base_url = "base"
+    resource_url = "/test/url"
+    target_url = "base{url}".format(base=base_url, url=resource_url)
+    session = Mock()
+    session.get_base_url2.return_value = base_url
+    responses.add(
+        responses.GET, target_url,
+    )
+
+    with patch.object(restv2helper._requests_session, "delete") as mock:
+        # Execute the request
+        _delete(session, resource_url)
+    # Should pass through to the correct session
+    mock.assert_called_with(
+        target_url,
+        headers=_get_headers(session),
+        params=None,
+        verify=session.verify_ssl_certs,
+    )
+
+
+@responses.activate
+def test_get():
+    """Make sure calls to _get end up using the correct session."""
+    base_url = "base"
+    resource_url = "/test/url"
+    target_url = "base{url}".format(base=base_url, url=resource_url)
+    session = Mock()
+    session.get_base_url2.return_value = base_url
+    responses.add(
+        responses.GET, target_url,
+    )
+
+    with patch.object(restv2helper._requests_session, "get") as mock:
+        # Execute the request
+        _get(session, resource_url, None)
+    # Should pass through to the correct session
+    mock.assert_called_with(
+        target_url,
+        headers=_get_headers(session),
+        params=None,
+        stream=False,
+        verify=session.verify_ssl_certs,
+    )
+
+
+@responses.activate
+def test_post():
+    """Make sure calls to _post end up using the correct session."""
+    base_url = "base"
+    resource_url = "/test/url"
+    target_url = "base{url}".format(base=base_url, url=resource_url)
+    session = Mock()
+    session.get_base_url2.return_value = base_url
+    responses.add(
+        responses.POST, target_url,
+    )
+    obj = "foo"
+
+    with patch.object(restv2helper._requests_session, "post") as mock:
+        # Execute the request
+        _post(session, resource_url, obj)
+    # Should pass through to the correct session
+    mock.assert_called_with(
+        target_url,
+        json=_encoder.default(obj),
+        headers=_get_headers(session),
+        params=None,
+        verify=session.verify_ssl_certs,
+    )
+
+
+@responses.activate
+def test_put():
+    """Make sure calls to _put end up using the correct session."""
+    base_url = "base"
+    resource_url = "/test/url"
+    target_url = "base{url}".format(base=base_url, url=resource_url)
+    session = Mock()
+    session.get_base_url2.return_value = base_url
+    responses.add(
+        responses.PUT, target_url,
+    )
+
+    with patch.object(restv2helper._requests_session, "put") as mock:
+        # Execute the request
+        _put(session, resource_url)
+    # Should pass through to the correct session
+    mock.assert_called_with(
+        target_url,
+        json=None,
+        data=None,
+        headers=_get_headers(session),
+        verify=session.verify_ssl_certs,
+        params=None,
+    )
 
 
 if __name__ == "__main__":
