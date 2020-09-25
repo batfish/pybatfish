aa204e6bb0679b98d27dc55de96b1e25ece09b4f
Todd Millstein <todd@cs.ucla.edu>
2020-09-25 08:41:09 -0700

    fix typechecking issue

--- pybatfish/datamodel/route.py
+++ pybatfish/datamodel/route.py
@@ -12,7 +12,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-from typing import Dict, Iterable, List, Optional, Text  # noqa: F401
+from typing import Any, Dict, Iterable, List, Optional, Text  # noqa: F401
 
 import attr
 
