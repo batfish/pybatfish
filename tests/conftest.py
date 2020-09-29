5d159174400e200d22fec79ac8ab0797863e4430
ratulm <ratul@intentionet.com>
2020-09-29 14:21:39 -0700

    Test on only newer versions (#669)
    * add validation for routingpolicyspec
    
    * add constant
    
    * add routing policy spec to conftest.py
    
    * test routing policy completion only in the dev version
    
    * Merge remote-tracking branch 'origin/master' into dev-test
    
    * fix docstring
    
    * delete constant
    
    * take out __
    
    * run right version of black
    
    * use session object

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,6 @@ COMPLETION_TYPES = [
     VariableType.PREFIX,
     VariableType.PROTOCOL,
     VariableType.REFERENCE_BOOK_NAME,
-    VariableType.ROUTING_POLICY_SPEC,
     VariableType.ROUTING_PROTOCOL_SPEC,
     VariableType.STRUCTURE_NAME,
     VariableType.VRF,
