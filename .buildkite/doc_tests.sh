cf08c0634c7a33cfbc8be1fef04e6aa8baf2d334
Daniel Halperin <dan@intentionet.com>
2020-09-30 12:42:12 -0700

    docs: only document stable questions

--- .buildkite/doc_tests.sh
+++ .buildkite/doc_tests.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 apt update -qq && apt -qq install -y openjdk-11-jre-headless
 tar -xzf workspace/questions.tgz
-java -cp workspace/allinone.jar org.batfish.allinone.Main -runclient false -coordinatorargs '-templatedirs questions -periodassignworkms=5' 2>&1 > workspace/batfish.log &
+java -cp workspace/allinone.jar org.batfish.allinone.Main -runclient false -coordinatorargs '-templatedirs questions/stable -periodassignworkms=5' 2>&1 > workspace/batfish.log &
 pip install -r docs/requirements.txt
 pip install -e .[dev] -q
 pytest docs
