af4786fbb57736bbf869ec98744109b476208147
Daniel Halperin <dan@intentionet.com>
2021-10-22 21:46:44 -0700

    restore imports

--- jupyter_notebooks/startup.py
+++ jupyter_notebooks/startup.py
@@ -6,7 +6,10 @@ import pandas as pd
 from IPython.display import display
 from pandas.io.formats.style import Styler
 
+from pybatfish.client.session import Session  # noqa: F401
+
 # noinspection PyUnresolvedReferences
+from pybatfish.datamodel import Edge, Interface  # noqa: F401
 from pybatfish.datamodel.answer import TableAnswer
 from pybatfish.datamodel.flow import HeaderConstraints, PathConstraints  # noqa: F401
 from pybatfish.datamodel.route import BgpRoute  # noqa: F401
