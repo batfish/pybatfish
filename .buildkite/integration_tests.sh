43829b2239ca1694ea3c89adf1f51bbdbb07546a
Ari Fogel <ari@fogelti.me>
2022-08-22 13:25:43 -0700

    Do not start Batfish WorkMgrV1 for regular integration tests (#853)

--- .buildkite/integration_tests.sh
+++ .buildkite/integration_tests.sh
@@ -6,7 +6,21 @@ version=$1
 
 apt update -qq && apt -qq install -y openjdk-11-jre-headless
 tar -xzf workspace/questions.tgz
-java -cp workspace/allinone.jar org.batfish.allinone.Main -runclient false -coordinatorargs '-templatedirs questions -periodassignworkms=5' 2>&1 > workspace/batfish.log &
+
+shift
+# remaining args are extra coordinator args
+coordinator_args=(\
+  -templatedirs=questions \
+  -periodassignworkms=5 \
+  "$@" \
+)
+
+allinone_args=(\
+  -runclient=false
+  -coordinatorargs="$(echo -n "${coordinator_args[@]}")" \
+)
+
+java -cp workspace/allinone.jar org.batfish.allinone.Main "${allinone_args[@]}" 2>&1 > workspace/batfish.log &
 pip install -e .[dev] -q
 pytest tests/integration
 
