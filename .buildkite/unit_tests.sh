0d845827a05bfc21f5141ff9a87c602d4ecb292e
Spencer Fraint <spencer@intentionet.com>
2022-08-30 12:42:09 -0700

    update cron schedule, remove old codecov stuff

--- .buildkite/unit_tests.sh
+++ .buildkite/unit_tests.sh
@@ -5,8 +5,3 @@ set -euo pipefail
 version=$1
 pip install -e .[dev]
 pytest tests 
-
-# Codecov handled in GitHub Actions now
-## Let codecov upload fail - codecov goes down not infrequently.
-#set +o pipefail
-#bash <(curl -s https://codecov.io/bash) -t 91216eec-ae5e-4836-8ee5-1d5a71d1b5bc -F unit${version//.}
