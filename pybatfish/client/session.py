c0aa7a7243470623b63f01806ab1c40363552dd4
Charles Prevot <charles.prevot@elca.ch>
2025-07-23 12:17:37 +0200

    Add type and docstring

--- pybatfish/client/session.py
+++ pybatfish/client/session.py
@@ -341,6 +341,8 @@ class Session:
     :ivar port_v2: Legacy alias for port
     :ivar ssl: Whether to use SSL when connecting to Batfish (False by default)
     :ivar api_key: Your API key
+    :ivar proxies: A dictionary of proxies to use for the session. See `requests` documentation for details.
+    :ivar timeout: Timeout for requests in seconds (default is 30 seconds)
     """
 
     def __init__(
@@ -355,7 +357,7 @@ class Session:
         verify_ssl_certs: bool = Options.verify_ssl_certs,
         api_key: str = CoordConsts.DEFAULT_API_KEY,
         load_questions: bool = True,
-        proxies=None,
+        proxies: Optional[Dict[str, str]] = None,
         timeout: Optional[int] = 30,
     ):
         # Coordinator args
